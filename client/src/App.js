f695b1a72d42b13d6622eaf7101fbe5ae4c94157
tarunbalaji1 <tarunbalajinidiganti@gmail.com>
2025-07-07 19:47:26 +0530

    feat: initial MERN + CSS boilerplate

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to WattWise</h1>
+      <button className="button">Check Usage</button>
+    </div>
+  );
+}
